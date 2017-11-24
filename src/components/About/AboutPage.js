dc360bbebbd46eab608410df1f7debae4fe7380d
ff_roy_solera <roy@firstfactory.com>
2017-11-24 16:22:05 -0600

    first commit

---
+++ src/components/About/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends from React.Component {
+    render() {
+        return(
+            <div className="jumbotron">
+                <h1>About</h1>
+                <p>some ttext</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HomePage extends from React.Component {
+    render() {
+        return(
+            <div className="jumbotron">
+                <h1>Pluralsight Adminis</h1>
+                <p>some ttext</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
